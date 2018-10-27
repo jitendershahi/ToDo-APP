5b51099b94b5201c1e2c30093d3d3e6a736c1d27
jitendershahi <jitendersharmasdec@gamil.com>
2018-10-27 13:01:58 +0530

    feature:import redux and thunk

--- src/index.js
+++ src/index.js
@@ -5,17 +5,30 @@ import App from './App';
 // import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import { BrowserRouter} from 'react-router-dom'
+import { BrowserRouter} from 'react-router-dom';
 // import Popper from 'popper.js';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const rootReducer = combineReducers({
+
+})
+
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+))
 // console.log($,Popper)
 
 const app = (
-    <BrowserRouter basename="/">
-        <App />
-    </BrowserRouter>
+    <Provider>
+        <BrowserRouter basename="/">
+            <App />
+        </BrowserRouter>
+    </Provider>
 )
 
 ReactDOM.render(app, document.getElementById('root'));
