02e77968ea9ac9b576c4ef7414a8eb651f9b7c9e
jitendershahi <jitendersharmasdec@gamil.com>
2018-10-27 12:15:56 +0530

    feature:import bootstrap and jquery

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Popper from 'popper.js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
